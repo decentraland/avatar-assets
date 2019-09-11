2dea9ca43fa4400acbcb0ce2a48b5b86dcde22b5
nobody <no@body.com>
2019-09-11 03:06:46 -0300

    folder cid

--- src/cid/getFileCID.ts
+++ src/cid/getFileCID.ts
@@ -3,8 +3,10 @@ import * as IPLD from 'ipld'
 import * as inMemory from 'ipld-in-memory'
 
 const first = require('async-iterator-first')
+const all = require('async-iterator-all')
 
 const inMemoryIPLD = inMemory(IPLD)
+
 export const getFileCID = async (buffer: Buffer, filename = '') => {
   const ipld = await inMemoryIPLD
   const file = await first(
@@ -20,4 +22,19 @@ export const getFileCID = async (buffer: Buffer, filename = '') => {
     )
   )
   return file.cid.toBaseEncodedString()
+}
+
+export const getFolderCID = async (flatFolder: { buffer: Buffer; filename: string }[]) => {
+  const ipld = await inMemoryIPLD
+  const folder = await all(importer(
+    flatFolder
+      .sort((a, b) => (a.filename < b.filename ? -1 : 1))
+      .map(_ => ({
+        path: './' + _.filename,
+        content: _.buffer
+      })),
+    ipld,
+    { strategy: 'flat' }
+  ))
+  return folder[folder.length-1].cid.toBaseEncodedString()
 }
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai'
 import * as fs from 'fs'
 import * as path from 'path'
-import { getFileCID } from './cid/getFileCID'
+import { getFileCID, getFolderCID } from './cid/getFileCID'
 import { getCategories } from './assets/getCategories'
 
 const expect = chai.expect
@@ -29,5 +29,15 @@ describe('process files', () => {
     const content = fs.readFileSync(path.resolve(__dirname, '..', 'README.md'))
     const cid = await getFileCID(content)
     expect(cid).to.eq('QmbiPLfzT76XqNU2fB4NfqkYSTMQkLShCQu9pt2NFoGM6k')
+  })
+  it('generates the CID of a folder', async () => {
+    const cid = await getFolderCID([{
+      filename: 'README.md',
+      buffer: fs.readFileSync(path.resolve(__dirname, '..', 'README.md'))
+    }, {
+      filename: 'LICENSE.md',
+      buffer: fs.readFileSync(path.resolve(__dirname, '..', 'LICENSE'))
+    }])
+    expect(cid).to.eq('QmULpXkrDsTcYgeJNKkvPjScbdTD7dhN6E6DCdB6kHUfHs')
   })
 })
