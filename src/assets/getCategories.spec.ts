be6895839f3fccbdf491c92ab941b3e6017b29f0
Esteban Ordano <esteban@decentraland.org>
2019-09-16 11:45:23 +0200

    restore assets

--- src/assets/getCategories.spec.ts
+++ src/assets/getCategories.spec.ts
@@ -5,18 +5,7 @@ describe('process files', () => {
   it('utility scanner returns a list of all the categories', () => {
     const categories = getCategories()
     expect(categories).to.deep.equal([
-      'body_shape',
-      'earring',
-      'eyebrows',
-      'eyes',
-      'eyewear',
-      'facial_hair',
-      'feet',
-      'hair',
-      'lower_body',
-      'mouth',
-      'tiara',
-      'upper_body'
+      'mask'
     ])
   })
 })
