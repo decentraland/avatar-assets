3ace301f29f24ebd8798f69330718e9320aeab12
Juan Scolari <12957692+juanscolari@users.noreply.github.com>
2022-01-28 14:33:16 -0300

    feat: implement deployment to content servers (#39)
    * chore: add legacy migration scripts
    
    * chore: add migration dependencies
    
    * chore: solve Typescript build errors
    
    * chore: remove old migration dependencies files
    
    * chore: take migration file from the filesystem instead of the legacy API
    
    * fix: fix temp directory cleanup
    
    * chore: get wearable files from filesystem instead of the legacy API
    
    * chore: remove asset uploading to S3
    
    * chore: remove originally failing test
    
    * chore: ignore log files
    
    * feat: integrate migration script into the main script
    
    * chore: modify script to run the deployment
    
    * chore: get asset directories dynamically
    
    * feat: pass `identityFilePath` as an argument
    
    * feat: remove --onlyBaseAvatars argument
    
    * feat: set --id argument as required
    
    * feat: remove target argument
    
    * chore: modify build command to only build
    
    * chore: split catalog and deploy scripts
    
    * docs: update Readme with new deployment methods
    
    * chore: remove unused mocha package
    
    * docs: add documentation about multiple ids passed
    
    * refactor: remove `V2Collection` type
    
    * chore: remove unused `expected.json` file
    
    * chore: remove index.html building
    
    * refactor: change wearables var name
    
    * refactor: pass wearables to the deploy script directly
    
    * refactor: rename migrate to deploy
    
    * refactor: move null wearables filter
    
    * refactor: remove progress bar for wearable conversion
    
    * chore: remove wearables logging to the console
    
    * refactor: separate v3 wearable pointers and metadata
    
    * refactor: remove V2Wearable type
    
    * refactor: rename V3Wearable type
    
    * chore: remove unused file `buildCatalog`
    
    * feat: remove build unused command
    
    * feat: remove unused catalog command
    
    * feat: deploy the assets by default
    
    * docs: update metadata build function description

---
+++ src/migration/utils.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs'
+import * as cliProgress  from 'cli-progress';
+import asyncPool from "tiny-async-pool"
+import * as EthCrypto from "eth-crypto"
+import { Authenticator } from 'dcl-crypto'
+import { ContentFileHash } from 'dcl-catalyst-commons';
+import { Identity } from './types';
+import { CONCURRENCY } from './config';
+import { Wearable } from '../types';
+
+export async function executeWithProgressBar<T, K>(detail: string, array: Array<T>, iterator: (T) => Promise<K>): Promise<K[]> {
+  const bar = new cliProgress.SingleBar({format: `${detail}: [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}`});
+  bar.start(array.length, 0);
+  const result = await asyncPool(CONCURRENCY, array, async (value) => {
+    const result: K = await iterator(value)
+    bar.increment(1)
+    return result
+  });
+  bar.stop()
+  return result
+}
+
+export async function parseIdentityFile(filePath: string): Promise<Identity> {
+  const buffer = await fs.promises.readFile(filePath)
+  return JSON.parse(buffer.toString())
+}
+
+export function getContentFileMap(wearable: Wearable): { key: string, hash: ContentFileHash}[] {
+  const contentFileMap: { key: string, hash: ContentFileHash}[] = [];
+  if (wearable.thumbnail) {
+    contentFileMap.push({ key: 'thumbnail.png', hash: wearable.thumbnail });
+  }
+  if (wearable.image) {
+    contentFileMap.push({ key: 'image.png', hash: wearable.image });
+  }
+  wearable.representations.forEach(representation =>
+    representation.contents.forEach(({ file, hash }) => {
+      if (file === 'thumbnail.png' || file === 'image.png') {
+        throw new Error(`Found a wearable with a content called either thumbnail or image ${wearable.id}`)
+      }
+      contentFileMap.push({ key: file, hash })
+    }))
+  return contentFileMap
+}
+
+export function sign(entityId: string, identity: Identity) {
+  const messageHash = Authenticator.createEthereumMessageHash(entityId)
+  return EthCrypto.sign(identity.privateKey, messageHash)
+}
+
+export function flatten<T>(array: T[][]): T[] {
+  return array.reduce((acc, val) => acc.concat(val), []);
+}
