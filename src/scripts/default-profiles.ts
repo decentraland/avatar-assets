b9bd5b524d0828fb6570935d6b4a34fc2c968077
Mariano Goldman <marianogoldman@users.noreply.github.com>
2024-05-07 15:45:54 -0300

    fix: add missing properties to all default profiles (#135)

--- src/scripts/default-profiles.ts
+++ src/scripts/default-profiles.ts
@@ -89,40 +89,32 @@ async function main() {
       body: ipfsHashes[1].hash
     }
 
-    if (!Profile.validate(metadata) && Profile.validate.errors) {
-      const filtered = Profile.validate.errors.filter(
-        (error) =>
-          error.params?.missingProperty !== 'ethAddress' &&
-          error.params?.missingProperty !== 'version' &&
-          error.params?.missingProperty !== 'tutorialStep'
-      )
-      if (filtered.length > 0) {
-        errors.push({
-          pointer,
-          validationResult: filtered.map((result) => result!.message).join(' ')
+    if (Profile.validate(metadata)) {
+      if (args.deploy) {
+        const deploymentData: DeploymentPreparationData = await DeploymentBuilder.buildEntity({
+          type: EntityType.PROFILE,
+          pointers: [pointer],
+          files,
+          metadata: { ...metadata }
         })
+
+        const { entityId } = deploymentData
+
+        const signature = ethSign(hexToBytes(privateKey), entityId)
+        const authChain = Authenticator.createSimpleAuthChain(entityId, ethAddress, signature)
+
+        await contentClient.deploy({
+          authChain,
+          entityId: deploymentData.entityId,
+          files: deploymentData.files
+        })
+        logger.info(`Default Profile ${pointer} from deployed`)
       }
-    }
-
-    if (args.deploy) {
-      const deploymentData: DeploymentPreparationData = await DeploymentBuilder.buildEntity({
-        type: EntityType.PROFILE,
-        pointers: [pointer],
-        files,
-        metadata: { ...metadata }
+    } else if (Profile.validate.errors) {
+      errors.push({
+        pointer,
+        validationResult: Profile.validate.errors.map((result) => result!.message).join(' ')
       })
-
-      const { entityId } = deploymentData
-
-      const signature = ethSign(hexToBytes(privateKey), entityId)
-      const authChain = Authenticator.createSimpleAuthChain(entityId, ethAddress, signature)
-
-      await contentClient.deploy({
-        authChain,
-        entityId: deploymentData.entityId,
-        files: deploymentData.files
-      })
-      logger.info(`Default Profile ${pointer} from default-profiles deployed`)
     }
   }
 
@@ -133,7 +125,7 @@ async function main() {
         errors.map((error) => `* **${error.pointer}**: ${error.validationResult}`).join('\n')
       )
     } else {
-      fs.writeFileSync('validation-errors.md', 'No validation errors.')
+      fs.writeFileSync('validation-errors.md', 'None.')
     }
     fs.writeFileSync('output', '')
   }
